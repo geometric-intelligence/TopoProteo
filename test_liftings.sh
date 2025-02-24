9401795d7d4a9dcddd156865c8f544a38d58164a
levtelyatnikov <tlscabinet@gmail.com>
2025-02-24 06:58:41 -0800

    adding g2h_discrete_configuration_complex

--- test_liftings.sh
+++ test_liftings.sh
@@ -2,12 +2,12 @@ project_name="LIFTING_TEST"
 DATASETS=('MUTAG') #'PROTEINS'
 
 # CELLULAR
-transforms_experiments=()
+transforms_experiments=(exp_cell/g2h_discrete_configuration_complex)
 
 # Date 24/02
 # working: 
-
-# NOTE 1:
+# Not working exp_cell/g2h_discrete_configuration_complex
+# NOTE 1: The discrete_configuration_complex has a
 
 
 for transform in ${transforms_experiments[*]}
@@ -15,7 +15,7 @@ do
     for dataset in ${DATASETS[*]}
     do
             python topobenchmark/run.py\
-            model=simplicial/cwn\
+            model=cell/cwn\
             dataset=graph/$dataset\
             trainer.max_epochs=2\
             trainer.min_epochs=1\
@@ -25,28 +25,26 @@ do
     done
 done
 
-transforms_experiments=(simplicial_experiments/g2h_neighborhood_complex)
+# ------------------------- SIMPLICIAL -------------------------
+# transforms_experiments=(exp_simplicial/g2h_neighborhood_complex)
+# # Date 24/02
+# # working: g2s_khop g2s_vietoris_rips g2h_neighborhood_complex g2s_graph_induceds g2s_dnd
 
+# # NOTE 1: the code of this submission is good but g2s_line (line lifting) construct line graph and finds graph induced topology. Hence the edges becomes nodes. THis lifting do not fits into our TB framework without modification 
+# # NOTE 2: it seems that g2s_graph_induced is our clique lifting. 
+# # NOTE 3: g2s_dnd is very slow as it generates enormous amout of simplices, even for small datasets.
 
-# SIMPLICIAL
-# Date 24/02
-# working: g2s_khop g2s_vietoris_rips g2h_neighborhood_complex g2s_graph_induceds g2s_dnd
-
-# NOTE 1: the code of this submission is good but g2s_line (line lifting) construct line graph and finds graph induced topology. Hence the edges becomes nodes. THis lifting do not fits into our TB framework without modification 
-# NOTE 2: it seems that g2s_graph_induced is our clique lifting. 
-# NOTE 3: g2s_dnd is very slow as it generates enormous amout of simplices, even for small datasets.
-
-for transform in ${transforms_experiments[*]}
-do
-    for dataset in ${DATASETS[*]}
-    do
-            python topobenchmark/run.py\
-            model=simplicial/scn\
-            dataset=graph/$dataset\
-            trainer.max_epochs=2\
-            trainer.min_epochs=1\
-            trainer.check_val_every_n_epoch=1\
-            transforms=$transform\
-            logger.wandb.project=$project_name
-    done
-done
+# for transform in ${transforms_experiments[*]}
+# do
+#     for dataset in ${DATASETS[*]}
+#     do
+#             python topobenchmark/run.py\
+#             model=simplicial/scn\
+#             dataset=graph/$dataset\
+#             trainer.max_epochs=2\
+#             trainer.min_epochs=1\
+#             trainer.check_val_every_n_epoch=1\
+#             transforms=$transform\
+#             logger.wandb.project=$project_name
+#     done
+# done
