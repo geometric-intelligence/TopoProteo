84a0148f17f237f46610828f8b5e73718e7d2f83
levtelyatnikov <tlscabinet@gmail.com>
2025-02-24 02:05:44 -0800

    g2s_vietoris_rips

--- test_liftings.sh
+++ test_liftings.sh
@@ -1,14 +1,19 @@
-DATASETS=('MUTAG' 'PROTEINS')
 project_name="LIFTING_TEST"
+DATASETS=('MUTAG' 'PROTEINS')
 
+transforms_experiments=(g2s_khop g2s_vietoris_rips)
 
+for transform in ${transforms_experiments[*]}
 for dataset in ${DATASETS[*]}
-do
-    python topobenchmark/run.py\
-    model=simplicial/scn\
-    dataset=graph/$dataset\
-    trainer.max_epochs=2\
-    trainer.min_epochs=1\
-    trainer.check_val_every_n_epoch=1\
-    transforms=g2s_khop
-done
+    do
+        do
+            python topobenchmark/run.py\
+            model=simplicial/scn\
+            dataset=graph/$dataset\
+            trainer.max_epochs=2\
+            trainer.min_epochs=1\
+            trainer.check_val_every_n_epoch=1\
+            transforms=$transform\
+            logger.wandb.project=$project_name
+        done
+    done
