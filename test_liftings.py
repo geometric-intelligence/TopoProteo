45f43ac00f0806b3e12a81016bf0903d40db144a
Coerulatus <marcomontagna21@gmail.com>
2025-02-26 17:39:21 +0000

    added references to commits of deleted liftings

--- test_liftings.py
+++ test_liftings.py
@@ -1,46 +1,37 @@
-project_name="LIFTING_TEST"
-DATASETS=('MUTAG') #'PROTEINS'
+"""Test liftings with python to be able to use the debugger more easily."""
 
+import subprocess
 
+# Configurations
+project_name = "LIFTING_TEST"
+datasets = ["geometric_shapes"]  # Add more datasets if needed
+transforms_experiments = ["exp_hypergraph/p2h_voronoi"]
 
-# # ------------------------- Graph2Combinatorial -------------------------
-# transforms_experiments=(exp_combinatorial/g2c_curve)
-# # Date 25/02 
-# # working: 
-# # Not working: exp_combinatorial/g2c_curve, exp_combinatorial/g2c_ring_close_atoms, exp_combinatorial/g2c_simplicial_paths
-# # NOTE 1: I'm pretty sure exp_combinatorial/g2c_curve is working but it is way too slow, even on MUTAG
-# # NOTE 2: exp_combinatorial/g2c_ring_close_atoms works only on data with data.smiles attribute, which is not the case for our datasets
-# # NOTE 3: exp_combinatorial/g2c_simplicial_paths is not working. They don't check if they find simplices of high enough ranks (they need cliques of adeguate size to be able to find them) so everything breaks.
-# # NOTE 4: last commit before deletion 2cc98ffaa64508dc22ba8dc381112c573c1dfa5c
-# # NOTE 5: commit when deleted 86e53397a5cec8aa891acfd1f2ea61f9af247502
+# Run the commands in Python
+for transform in transforms_experiments:
+    for dataset in datasets:
+        command = [
+            "python",
+            "topobenchmark/run.py",
+            f"model=hypergraph/unignn2",
+            f"dataset=pointcloud/{dataset}",
+            f"trainer.max_epochs=2",
+            f"trainer.min_epochs=1",
+            f"trainer.check_val_every_n_epoch=1",
+            f"transforms={transform}",
+            f"logger.wandb.project={project_name}",
+        ]
 
-
-
-# for transform in ${transforms_experiments[*]}
-# do
-#     for dataset in ${DATASETS[*]}
-#     do
-#             python topobenchmark/run.py\
-#             model=cell/topotune\
-#             dataset=graph/$dataset\
-#             trainer.max_epochs=2\
-#             trainer.min_epochs=1\
-#             trainer.check_val_every_n_epoch=1\
-#             transforms=$transform\
-#             logger.wandb.project=$project_name
-#             #trainer.devices=\[$device\]
-#     done
-# done
+        print(f"Running: {' '.join(command)}")
+        subprocess.run(command)
 
 
 # # ------------------------- Hypergraph2Simplicial -------------------------
 # transforms_experiments=(exp_simplicial/h2s_heat)
-# # Date 25/02 
-# # working: 
+# # Date 25/02
+# # working:
 # # Not working exp_simplicial/h2s_heat
 # # NOTE 1: exp_simplicial/h2s_heat is not working. We can remove the dependency from the external library but the lifting doesn't return a simplicial complex but a torch_geometric.data.Data object.
-# # NOTE 2: last commit before deletion 2cc98ffaa64508dc22ba8dc381112c573c1dfa5c
-# # NOTE 3: commit when deleted 86e53397a5cec8aa891acfd1f2ea61f9af247502
 
 # for transform in ${transforms_experiments[*]}
 # do
@@ -61,10 +52,10 @@ DATASETS=('MUTAG') #'PROTEINS'
 
 # # ------------------------- Graph2Hypergraph -------------------------
 # transforms_experiments=(exp_hypergraph/g2h_forman_ricci_curvature exp_hypergraph/g2h_expander_graph g2h_kernel  exp_hypergraph/g2h_khop g2h_knn exp_hypergraph/g2h_mapper exp_hypergraph/g2h_modularity_maximization)
-# # Date 25/02 
+# # Date 25/02
 # # working: exp_hypergraph/g2h_forman_ricci_curvature exp_hypergraph/g2h_expander_graph g2h_kernel  exp_hypergraph/g2h_khop g2h_knn exp_hypergraph/g2h_mapper exp_hypergraph/g2h_modularity_maximization
-# # Not working 
-# # NOTE 1: 
+# # Not working
+# # NOTE 1:
 
 
 # for transform in ${transforms_experiments[*]}
@@ -112,8 +103,8 @@ DATASETS=('MUTAG') #'PROTEINS'
 # # Date 24/02
 # # working: g2s_khop g2s_vietoris_rips g2h_neighborhood_complex g2s_graph_induceds g2s_dnd
 
-# # NOTE 1: the code of this submission is good but g2s_line (line lifting) construct line graph and finds graph induced topology. Hence the edges becomes nodes. THis lifting do not fits into our TB framework without modification 
-# # NOTE 2: it seems that g2s_graph_induced is our clique lifting. 
+# # NOTE 1: the code of this submission is good but g2s_line (line lifting) construct line graph and finds graph induced topology. Hence the edges becomes nodes. THis lifting do not fits into our TB framework without modification
+# # NOTE 2: it seems that g2s_graph_induced is our clique lifting.
 # # NOTE 3: g2s_dnd is very slow as it generates enormous amout of simplices, even for small datasets.
 
 # for transform in ${transforms_experiments[*]}
@@ -130,28 +121,3 @@ DATASETS=('MUTAG') #'PROTEINS'
 #             logger.wandb.project=$project_name
 #     done
 # done
-
-# ------------------------- Pointcloud2Hypergraph -------------------------
-transforms_experiments=(exp_hypergraph/p2h_mogmst exp_hypergraph/p2h_pointnet exp_hypergraph/p2h_voronoi)
-DATASETS=('geometric_shapes')
-# Date 25/02 
-# working:exp_hypergraph/p2h_mogmst exp_hypergraph/p2h_pointnet exp_hypergraph/p2h_voronoi
-# Not working: 
-# NOTE 1: 
-
-
-for transform in ${transforms_experiments[*]}
-do
-    for dataset in ${DATASETS[*]}
-    do
-            python topobenchmark/run.py\
-            model=hypergraph/unignn2\
-            dataset=pointcloud/$dataset\
-            trainer.max_epochs=2\
-            trainer.min_epochs=1\
-            trainer.check_val_every_n_epoch=1\
-            transforms=$transform\
-            logger.wandb.project=$project_name
-            #trainer.devices=\[$device\]
-    done
-done
