0c4cdf1f35edf75922bb01a626a14da6caa43786
Louisa Cornelis <cornelislouisa@gmail.com>
2025-06-04 18:09:02 -0700

    Modifying the readout by removing scatter but adding a graph encoder prior to final readout

--- topobench/nn/readouts/ftd_readout.py
+++ topobench/nn/readouts/ftd_readout.py
@@ -22,47 +22,41 @@ class FTDReadOut(AbstractZeroCellReadOut):
         fc_act=None,
         out_channels=None,
         use_feature_encoder=True,
+        feature_output_dim=None, #64
+        graph_encoder_dim=None, #256
         **kwargs,
     ):
         super().__init__(out_channels=out_channels, **kwargs)
         self.hidden_dim = kwargs["hidden_dim"]
-        self.feature_output_dim = (
-            self.hidden_dim
-        )  # how big do you want sex, mutation, age to be encoded to
+        self.feature_output_dim = feature_output_dim
+        self.graph_encoder_dim = graph_encoder_dim
         self.which_layer = which_layer
         self.fc_dim = fc_dim
         self.fc_dropout = fc_dropout
         self.fc_act = fc_act
-        self.fc_input_dim = (
-            self.hidden_dim * 3 * 2
-        )  # how big are graph features + sex, mutation, age
+        self.fc_input_dim = graph_encoder_dim + feature_output_dim * 3
         self.out_channels = out_channels  # 1
         self.use_feature_encoder = use_feature_encoder
         self.readout_layers = self.build_readout_layers()
         self.feature_encoder = self.build_feature_encoder()
+        self.graph_encoder = self.build_graph_encoder()
 
-    def scatter_node_features(self, x, batch):
-        """Scatter node features to graph level.
+    def build_graph_encoder(self):
+        return nn.Linear(self.hidden_dim, self.graph_encoder_dim)
+   
+    def build_feature_encoder(self):
+        if self.use_feature_encoder:
+            return nn.Linear(1, self.feature_output_dim)
+        else:
+            return nn.Identity()
 
-        Parameters
-        ----------
-        x : torch.Tensor
-            Node features.
-        batch : torch_geometric.data.Batch
-            Batch object containing the batched data.
-
-        Returns
-        -------
-        torch.Tensor
-            Graph-level features.
-        """
-        scatter_min = scatter(x, batch, dim=0, reduce="min")
-        scatter_mean = scatter(x, batch, dim=0, reduce="mean")
-        scatter_max = scatter(x, batch, dim=0, reduce="max")
-        concat_scatter = torch.cat(
-            [scatter_min, scatter_mean, scatter_max], dim=1
-        )
-        return concat_scatter
+    def encode_features(self, data):
+        encoded_features = []
+        for feature in self.which_layer:
+            if feature in ["sex", "mutation", "age"]:
+                feature_value = data.get(feature)
+                encoded_features.append(self.feature_encoder(feature_value))
+        return torch.cat(encoded_features, dim=1)
 
     def build_readout_layers(self):
         layers = []
@@ -79,35 +73,16 @@ class FTDReadOut(AbstractZeroCellReadOut):
         layers.append(nn.Linear(fc_dim, self.out_channels))
         return nn.Sequential(*layers)
 
-    def build_feature_encoder(self):
-        if self.use_feature_encoder:
-            return nn.Linear(1, self.feature_output_dim)
-        else:
-            return nn.Identity()
-
-    def encode_features(self, data):
-        sex = data.sex
-        mutation = data.mutation
-        age = data.age
-        encoded_features = []
-        for feature in self.which_layer:
-            if feature in ["sex", "mutation", "age"]:
-                feature_value = locals().get(feature)
-                encoded_features.append(self.feature_encoder(feature_value))
-        return torch.cat(encoded_features, dim=1)
-
     def concatenate_features(self, graph_features, demographic_features):
         batch_size = demographic_features.shape[0]
         graph_features = graph_features.view(batch_size, -1)
         return torch.cat([graph_features, demographic_features], dim=1)
 
     def forward(self, model_out, batch):
-        scaterred_features = self.scatter_node_features(
-            model_out["x_0"], batch["batch_0"]
-        )
+        encoded_graph = self.graph_encoder(model_out["x_0"])
         demographic_features = self.encode_features(batch)
         total_features = self.concatenate_features(
-            scaterred_features, demographic_features
+            encoded_graph, demographic_features
         )
         model_out["x_0"] = self.readout_layers(total_features)
         return model_out
