4c4ee1b26a79e777a07874e68461695b5bee26f4
Guillermo <guille_gbg@hotmail.com>
2025-10-13 16:44:23 -0700

    Prerun script

---
+++ topoproteo_prerun.sh
@@ -0,0 +1,89 @@
+python -m topobench \
+    dataset=graph/FTD \
+    model=graph/mlp \
+    dataset.loader.parameters.adj_metric=wgcna \
+    dataset.loader.parameters.adj_thresh=0.2,0.25,0.3 \
+    dataset.loader.parameters.kfold=true \
+    dataset.loader.parameters.num_folds=5 \
+    dataset.loader.parameters.fold=0 \
+    dataset.split_params.data_seed=0 \
+    trainer.max_epochs=2 \
+    trainer.min_epochs=1 \
+    trainer.check_val_every_n_epoch=1 \
+    trainer.devices=\[0\] \
+    logger.wandb.project=prerun \
+    callbacks.early_stopping.patience=50 \
+    tags="[TopoProteoGridSearch]" \
+    --multirun &
+
+python -m topobench \
+    dataset=graph/FTD \
+    model=graph/mlp \
+    dataset.loader.parameters.adj_metric=wgcna \
+    dataset.loader.parameters.adj_thresh=0.2,0.25,0.3 \
+    dataset.loader.parameters.kfold=true \
+    dataset.loader.parameters.num_folds=5 \
+    dataset.loader.parameters.fold=1 \
+    dataset.split_params.data_seed=0 \
+    trainer.max_epochs=2 \
+    trainer.min_epochs=1 \
+    trainer.check_val_every_n_epoch=1 \
+    trainer.devices=\[0\] \
+    logger.wandb.project=prerun \
+    callbacks.early_stopping.patience=50 \
+    tags="[TopoProteoGridSearch]" \
+    --multirun &
+
+python -m topobench \
+    dataset=graph/FTD \
+    model=graph/mlp \
+    dataset.loader.parameters.adj_metric=wgcna \
+    dataset.loader.parameters.adj_thresh=0.2,0.25,0.3 \
+    dataset.loader.parameters.kfold=true \
+    dataset.loader.parameters.num_folds=5 \
+    dataset.loader.parameters.fold=2 \
+    dataset.split_params.data_seed=0 \
+    trainer.max_epochs=2 \
+    trainer.min_epochs=1 \
+    trainer.check_val_every_n_epoch=1 \
+    trainer.devices=\[0\] \
+    logger.wandb.project=prerun \
+    callbacks.early_stopping.patience=50 \
+    tags="[TopoProteoGridSearch]" \
+    --multirun &
+
+python -m topobench \
+    dataset=graph/FTD \
+    model=graph/mlp \
+    dataset.loader.parameters.adj_metric=wgcna \
+    dataset.loader.parameters.adj_thresh=0.2,0.25,0.3 \
+    dataset.loader.parameters.kfold=true \
+    dataset.loader.parameters.num_folds=5 \
+    dataset.loader.parameters.fold=3 \
+    dataset.split_params.data_seed=0 \
+    trainer.max_epochs=2 \
+    trainer.min_epochs=1 \
+    trainer.check_val_every_n_epoch=1 \
+    trainer.devices=\[3\] \
+    logger.wandb.project=prerun \
+    callbacks.early_stopping.patience=50 \
+    tags="[TopoProteoGridSearch]" \
+    --multirun &
+
+python -m topobench \
+    dataset=graph/FTD \
+    model=graph/mlp \
+    dataset.loader.parameters.adj_metric=wgcna \
+    dataset.loader.parameters.adj_thresh=0.2,0.25,0.3 \
+    dataset.loader.parameters.kfold=true \
+    dataset.loader.parameters.num_folds=5 \
+    dataset.loader.parameters.fold=4 \
+    dataset.split_params.data_seed=0 \
+    trainer.max_epochs=2 \
+    trainer.min_epochs=1 \
+    trainer.check_val_every_n_epoch=1 \
+    trainer.devices=\[4\] \
+    logger.wandb.project=prerun \
+    callbacks.early_stopping.patience=50 \
+    tags="[TopoProteoGridSearch]" \
+    --multirun &
